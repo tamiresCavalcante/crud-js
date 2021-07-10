64a5dd8f4ddfb2f9074a2fda8505750903e6fa9c
Tamires Cavalcante <tmires.cavalcante@gmail.com>
2021-07-10 14:45:19 -0300

    Crud inicial

--- main.js
+++ main.js
@@ -6,6 +6,43 @@ const openModal = () => document.getElementById('modal')
 const closeModal = () => document.getElementById('modal')
     .classList.remove('active')
 
+const tempClient = {
+    nome: "Fernado",
+    email: "fernando@gmail.com",
+    celular: "21123456789",
+    cidade: "São Roque"
+}
+
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? [];
+const setLocalStorage = (dbClient) => localStorage.setItem("db_client", JSON.stringify(dbClient));
+
+//CRUD
+
+//delete
+const deleteClient = (index) => {
+    const dbClient = readClient()
+    dbClient.splice(index, 1)
+    setLocalStorage(dbClient)
+}
+
+//Update
+const updateClient = (index, client) => {
+    const dbClient = readClient()
+    dbClient[index] = client
+    setLocalStorage(dbClient)
+}
+
+// Read
+const readClient = () => getLocalStorage();
+
+//Create
+const createClient = (client) => {
+    const dbClient = getLocalStorage()
+    dbClient.push(client)
+    setLocalStorage(dbClient)
+}
+
+//Events
 document.getElementById('cadastrarCliente')
     .addEventListener('click', openModal)
 
